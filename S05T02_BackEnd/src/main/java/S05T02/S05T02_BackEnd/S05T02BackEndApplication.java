92c50cc5d9db403053fe0fc6f84d17103d655ea0
Santiago Hernandez <santiagohernandez@MacBook-Air.local>
2025-01-21 11:17:25 +0100

    Init commit

---
+++ S05T02_BackEnd/src/main/java/S05T02/S05T02_BackEnd/S05T02BackEndApplication.java
@@ -0,0 +1,13 @@
+package S05T02.S05T02_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T02BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02BackEndApplication.class, args);
+	}
+
+}
